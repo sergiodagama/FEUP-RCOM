c7782fcd394de05417ff920a5c32663b8139f029
N0il <sergio.r.gama@gmail.com>
2021-11-14 22:59:50 +0000

    finished byte stuffing function and left in api main a testing case

--- src/api.c
+++ src/api.c
@@ -1,96 +1,78 @@
-#define ESCAPE 0x7d
-#define FLAG 0x7e
-#define VALUE_TO_XOR 0x20
+#include "api.h"
 
-#include <stdio.h>
-#include <stdlib.h>
+char* byteStuffing(size_t size, unsigned char data[], size_t* trimmed_size){
 
-int byteStuffing(size_t size, unsigned char data[], unsigned char stuffedData[]){
-
+    if(size < 1){
+        perror("Error: Size inferior to one");
+        //return -1;
+    }
+    if(data == NULL){
+        perror("Error: null array pointers");
+        //return -1;
+    }
     int index = 1;
 
-    unsigned char xored1 = VALUE_TO_XOR ^ FLAG;
+    unsigned char stuffedRaw[size*2];
 
-    unsigned char xored2 = VALUE_TO_XOR ^ ESCAPE;
+    stuffedRaw[0] = FLAG;
 
-    printf("XORED1: %x\n", xored1);
-    printf("XORED2: %x\n\n", xored2);
-
-    //size_t size = sizeof data / sizeof data[0];
-
-    printf("SIZE: %u\n", size);
-
-    stuffedData[0] = FLAG;
-
-    //go through all the data array
-    for(int i = 1; i < size - 1; i++){
-        printf("%d\n", i);
+    //checking for escapes and flags in data
+    for(int i = 1; i < size-1; i++){
         if(data[i] == FLAG){
-            printf("flag: %d\n", i);
-            stuffedData[index] = ESCAPE;
-            stuffedData[index+1] = xored1;
-            index +=2;
+            stuffedRaw[index] = ESCAPE;
+            stuffedRaw[index+1] = FLAG_XORED;
+            index+=2;
         }
         else if(data[i] == ESCAPE){
-            printf("escape: %d\n", i);
-            stuffedData[index]  = ESCAPE;
-            stuffedData[index+1]  = xored2;
-            index += 2;
+            stuffedRaw[index] = ESCAPE;
+            stuffedRaw[index+1] = ESCAPE_XORED;
+            index+=2;
         }
         else{
-            stuffedData[index]  = data[i];
+            stuffedRaw[index]  = data[i];
             index++;
         }
-        stuffedData[index] = FLAG;
+        stuffedRaw[index] = FLAG;
     }
-    return index;  //check for malloc erros later
-}
 
+    //trimming the stuffed array 
+    unsigned char* stuffedTrimmed = malloc(index+1);
+
+    for(int i=0; i < index+1; i++){
+        stuffedTrimmed[i] = stuffedRaw[i];
+        (*trimmed_size)++;
+    }
+    return stuffedTrimmed;
+}
 
 //just for testing purposes
 int main(){
 
     unsigned char data[20] = {0x7e, 0x02, 0x03, 0x7e, 0x05, 0x06, 0x7e, 0x08, 0x7d, 0x10, 0x11, 0x12, 0x13, 0x14, 0x7e, 0x7d, 0x17, 0x7e, 0x19, 0x7e};
 
-    //unsigned char data[20] = {0x7e, 0x02, 0x03, 0x7e, 0x05, 0x06, 0x7e};
-
     size_t size = sizeof data / sizeof data[0];
-
-    printf("SIZE OUT: %u\n", size);
     
-    printf("DATA BEFORE STUFFING\n\n");
+    printf("DATA BEFORE STUFFING\n");
+    printf("Data Size: %u\n\n", size);
 
+    printf("Trama: ");
     for(int i = 0; i < sizeof(data); i++){
         printf("%x ", data[i]);
     }
-
-    printf("\n\n");
-
-      //because the worst case all the characters have to be stuffed
-    unsigned char stuffedData[size*2];
-
-    int sizeOf_buffed = byteStuffing(size, data, stuffedData);
-     
-    unsigned char trimed[sizeOf_buffed + 1];
-
-    for(int i =0; i < sizeOf_buffed + 1; i++){
-        trimed[i] = stuffedData[i];
-    }
-
-
-    printf("DATA AFTER STUFFING\n\n");
-
-    for(int i = 0; i < sizeof(stuffedData); i++){
-        printf("%x ", *(stuffedData + i));
-    }
-
     printf("\n");
 
-        printf("DATA AFTER TRIMED\n\n");
+    size_t final_size;
 
-    for(int i = 0; i < sizeof(trimed); i++){
-        printf("%x ", *(trimed + i));
+    unsigned char* stuffed = byteStuffing(size, data, &final_size);
+
+    if(stuffed == NULL) perror("Error: error on byteStuffing function");
+
+    printf("\nDATA AFTER STUFFING\n");
+    printf("Trimed Size: %u \n\n", final_size);
+
+    printf("Trama: ");
+    for(int i = 0; i < final_size; i++){
+        printf("%x ", *(stuffed + i));
     }
-
     return 0;
 }
--- src/api.h
+++ src/api.h
@@ -1,6 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 enum flag {TRANSMITTER, RECEIVER};
 
-int llopen(int porta, flag);
+//int llopen(int porta, flag);
 
 int llread(int fd, char * buffer);
 
@@ -8,4 +12,10 @@ int llwrite(int fd, char * buffer, int length);
 
 int llclose(int fd);
 
-int byteStuffing(unsigned char data[]);
+#define ESCAPE 0x7d
+#define FLAG 0x7e
+#define VALUE_TO_XOR 0x20
+#define FLAG_XORED (VALUE_TO_XOR ^ FLAG)
+#define ESCAPE_XORED (VALUE_TO_XOR ^ ESCAPE)
+
+char* byteStuffing(size_t size, unsigned char data[], size_t* trimmed_size);
